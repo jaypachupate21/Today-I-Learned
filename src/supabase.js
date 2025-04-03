7eac513c7926b8956bef53e942bc2ac92e5e2564
Jay Avinash Pachupate <pachupatejay2102@gmail.com>
2025-04-03 19:23:58 +0530

    removing api key from github

--- src/supabase.js
+++ src/supabase.js
@@ -1,7 +1,10 @@
-
 import { createClient } from '@supabase/supabase-js';
+
+const apiKey=process.env.REACT_APP_API_KEY;
+
 const supabaseUrl = 'https://fjncrerhxdydpmglafok.supabase.co';
-const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZqbmNyZXJoeGR5ZHBtZ2xhZm9rIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjcwMTQ0OTgsImV4cCI6MjA0MjU5MDQ5OH0.hRsfBHa8JfzDtie415MtYSKzUBs0EtFx1geCK7CNIJ0";
+
+const supabaseKey = apiKey;
 const supabase = createClient(supabaseUrl, supabaseKey);
 
 export default supabase;
